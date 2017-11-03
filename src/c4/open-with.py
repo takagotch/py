e3364f9a927360d72a294640e12d6f56a94f4163
takagotch <dyaccb@gmail.com>
2017-11-04 02:48:54 +0900

    py_ex

---
+++ src/c4/open-with.py
@@ -0,0 +1,4 @@
+with open("test.txt", mode="w") as f:
+    f.write("私は失敗したことがない。\n")
+    f.write("ただ、一万通りの方法を\n見つけただけだ。\n")
+
---
+++ src/c4/test-syaku.py
@@ -0,0 +1,10 @@
+# モジュールのインポート
+import syaku;
+
+# モジュールの関数を使う
+v = syaku.syaku_to_cm(10)
+print("10尺=", v, "cm")
+
+v = syaku.syaku_to_cm(20)
+print("20尺=", v, "cm")
+
